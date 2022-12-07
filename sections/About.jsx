fc96c06cd2f87c153d59eeb28321447bd956f448
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-12-07 13:58:13 +0300

    add down arrow

--- sections/About.jsx
+++ sections/About.jsx
@@ -21,6 +21,12 @@ const About = () => (
         variants={fadeIn('up', 'tween', 0.2, 1)}>
         <span className='font-extrabold text-white'>Metaverse</span> is a new thing in the future, where you can enjoy the virtual world by feeling like it's really real, you can feel what you feel in this metaverse world, because this is really the <span className='font-extrabold text-white'>madness of the metaverse</span> of today, using only <span className='font-extrabold text-white'>VR</span> devices you can easily explore the metaverse world you want, turn your dreams into reality. Let's <span className='font-extrabold text-white'>explore</span> the madness of the metaverse by scrolling down
       </motion.p>
+      <motion.img
+        className="w-[18px] h-[28px] object-contain mt-[28px]"
+        variants={fadeIn('up', 'tween', 0.3, 1)}
+        src="/arrow-down.svg"
+        alt="arrow down"
+      />
     </motion.div>
   </section>
 );
