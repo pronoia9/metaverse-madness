c8da256ddae195fdbb5f7f8a517b0577dde11150
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-12-07 13:46:35 +0300

    add imports

--- sections/About.jsx
+++ sections/About.jsx
@@ -1,5 +1,11 @@
 'use client';
 
+import { motion } from 'framer-motion';
+import { TypingText } from '../components';
+
+import styles from '../styles';
+import { fadeIn, staggerContainer } from '../utils/motion';
+
 const About = () => (
   <section>
     About section
