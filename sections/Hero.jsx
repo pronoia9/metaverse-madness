f82ac48c6347c6902eefed7b91bbb30257d588dd
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-12-07 13:44:46 +0300

    add stamp

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -27,10 +27,19 @@ const Hero = () => (
       <motion.div className='relative w-full md:-mt-[20px] -mt-[12px]' variants={slideIn('right', 'tween', 0.2, 1)}>
         <div className='absolute w-full h-[300px] hero-gradient rounded-tl-[140px] z-[0] -top-[30px]' />
         <img
+          className='w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-10 relative'
           src='/cover.png'
           alt='hero_cover'
-          className='w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-10 relative'
         />
+        <a href='#explore'>
+          <div className='w-full flex justify-end sm:-mt-[70px] -mt-[50px] pr-[40px] relative z-10'>
+            <img
+              className='sm:w-[155px] w-[100px] sm:h-[155px] h-[100px] object-contain'
+              src='/stamp.png'
+              alt='stamp'
+            />
+          </div>
+        </a>
       </motion.div>
     </motion.div>
   </section>
