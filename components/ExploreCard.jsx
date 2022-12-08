5a24e0f6de13672c9a067c8dfd9800873b509a34
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-12-08 13:21:56 +0300

    add imports

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -1,5 +1,10 @@
 'use client';
 
+import { motion } from 'framer-motion';
+
+import { fadeIn } from '../utils/motion';
+import styles from '../styles';
+
 const ExploreCard = () => (
   <div>
     Explore Card
