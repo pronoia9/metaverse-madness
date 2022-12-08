75215eff1c48d13319fb01a2810320497de8da31
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-12-08 13:21:25 +0300

    add cards / finish explore

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -8,21 +8,29 @@ import { staggerContainer } from '../utils/motion';
 import { ExploreCard, TitleText, TypingText } from '../components';
 import { exploreWorlds } from '../constants';
 
-const Explore = () => (
-  <section id='explore' className={`${styles.paddings}`}>
-    <motion.div
-      className={`${styles.innerWidth} mx-auto flex flex-col`}
-      variants={staggerContainer}
-      initial='hidden'
-      whileInView='show'
-      viewport={{ once: false, amount: 0.25 }}>
-      <TypingText title='| The World' textStyles='text-center' />
-      <TitleText
-        title={<>Choose the world you want <br className='md:block hidden' /> to explore</>}
-        textStyles='text-center'
-      />
-    </motion.div>
-  </section>
-);
+const Explore = () => {
+  const [active, setActive] = useState('world-2');
 
+  return (
+    <section id='explore' className={`${styles.paddings}`}>
+      <motion.div
+        className={`${styles.innerWidth} mx-auto flex flex-col`}
+        variants={staggerContainer}
+        initial='hidden'
+        whileInView='show'
+        viewport={{ once: false, amount: 0.25 }}>
+        <TypingText title='| The World' textStyles='text-center' />
+        <TitleText
+          title={<>Choose the world you want <br className='md:block hidden' /> to explore</>}
+          textStyles='text-center'
+        />
+        <div className='mt-[50px] flex lg:flex-row flex-col min-h-[70vh] gap-5'>
+          {exploreWorlds.map((world, index) => (
+            <ExploreCard key={world.id} {...world} index={index} active={active} handleClick={setActive} />
+          ))}
+        </div>
+      </motion.div>
+    </section>
+  );
+};
 export default Explore;
