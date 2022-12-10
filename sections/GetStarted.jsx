deed8a42e88b3de41d692d56cdb564f6db72e3bf
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-12-10 12:09:57 +0300

    finish get started

--- sections/GetStarted.jsx
+++ sections/GetStarted.jsx
@@ -22,6 +22,11 @@ const GetStarted = () => (
       <motion.div className='flex-[0.75] flex justify-center flex-col' variants={fadeIn('left', 'tween', 0.2, 1)}>
         <TypingText title='| How Metaversus Works' />
         <TitleText title={<>Get started with just a few clicks</>} />
+        <div className='mt-[31px] flex flex-col max-w-[370px] gap-[24px]'>
+          {startingFeatures.map((feature, index) => (
+            <StartSteps key={feature} number={`${index < 10 ? '0' : ''} ${index + 1}`} text={feature} />
+          ))}
+        </div>
       </motion.div>
     </motion.div>
   </section>
