48c98aa792c5cc3f1df7605a3437eb6a6703c434
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-12-10 12:17:04 +0300

    add title + text

--- sections/WhatsNew.jsx
+++ sections/WhatsNew.jsx
@@ -8,8 +8,23 @@ import { NewFeatures, TitleText, TypingText } from '../components';
 import { planetVariants, staggerContainer, fadeIn } from '../utils/motion';
 
 const WhatsNew = () => (
-  <section>
-    What's new section
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-8`}
+      variants={staggerContainer}
+      initial='hidden'
+      whileInView='show'
+      viewport={{ once: false, amount: 0.25 }}>
+      <motion.div className='flex-[0.95] flex justify-center flex-col' variants={fadeIn('right', 'tween', 0.2, 1)}>
+        <TypingText title='| Whats new?' />
+        <TitleText title={<>What's new about Metaversus?</>} />
+        <div className='mt-[48px] flex flex-wrap justify-between gap-[24px]'>
+          {newFeatures.map((feature) => (
+            <NewFeatures key={feature.title} {...feature} />
+          ))}
+        </div>
+      </motion.div>
+    </motion.div>
   </section>
 );
 
