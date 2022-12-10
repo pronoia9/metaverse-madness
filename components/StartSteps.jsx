e2853b9942a1ac4e35b0a522f3d33afec3fc89bc
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-12-10 12:11:28 +0300

    add index/numbers

--- components/StartSteps.jsx
+++ components/StartSteps.jsx
@@ -1,6 +1,10 @@
-const StartSteps = () => (
-  <div>
-    start steps
+import styles from '../styles';
+
+const StartSteps = ({ number, text }) => (
+  <div className={`${styles.flexCenter} flex-row`}>
+    <div className={`${styles.flexCenter} w-[70px] h-[70px] rounded-[24px] bg-[#323F5D]`}>
+      <p className='font-bold text-[20px] text-white'>{number}</p>
+    </div>
   </div>
 );
 
