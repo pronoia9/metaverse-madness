585c29cc2a3d54f92087479f26fbcea4c5572df0
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-12-10 12:24:13 +0300

    add image

--- components/InsightCard.jsx
+++ components/InsightCard.jsx
@@ -4,10 +4,10 @@ import { motion } from 'framer-motion';
 
 import { fadeIn } from '../utils/motion';
 
-const InsightCard = () => (
-  <div>
-    Insight Card
-  </div>
+const InsightCard = ({ imgUrl, title, subtitle, index }) => (
+  <motion.div className='flex md:flex-row flex-col gap-4' variants={fadeIn('up', 'spring', index * 0.5, 1)}>
+    <img className='md:w-[270px] w-full h-[250px] rounded-[32px] object-cover' src={imgUrl} alt='planet-01' />
+  </motion.div>
 );
 
 export default InsightCard;
