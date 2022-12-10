189bec484fb4795c9c27d822d0b9e80fcaa02b59
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-12-10 12:29:03 +0300

    add title

--- sections/World.jsx
+++ sections/World.jsx
@@ -7,8 +7,15 @@ import { TitleText, TypingText } from '../components';
 import { fadeIn, staggerContainer } from '../utils/motion';
 
 const World = () => (
-  <section>
-    World section
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+      variants={staggerContainer}
+      initial='hidden'
+      whileInView='show'
+      viewport={{ once: false, amount: 0.25 }}>
+      <TypingText title='| People on the World' textStyles='text-center' />
+    </motion.div>
   </section>
 );
 
