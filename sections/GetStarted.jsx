c7be38caf1c2ee8088c50615341564101d2d5b09
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-12-10 12:07:08 +0300

    add image

--- sections/GetStarted.jsx
+++ sections/GetStarted.jsx
@@ -7,6 +7,19 @@ import { startingFeatures } from '../constants';
 import { StartSteps, TitleText, TypingText } from '../components';
 import { staggerContainer, fadeIn, planetVariants } from '../utils/motion';
 
-const GetStarted = () => <section>Get Started Section</section>;
+const GetStarted = () => (
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-8`}
+      variants={staggerContainer}
+      initial='hidden'
+      whileInView='show'
+      viewport={{ once: false, amount: 0.25 }}>
+      <motion.div className={`flex-1 ${styles.flexCenter}`} variants={planetVariants('left')}>
+        <img className='w-[90%] h-[90%] object-contain' src='/get-started.png' alt='get-started' />
+      </motion.div>
+    </motion.div>
+  </section>
+);
 
 export default GetStarted;
