2d880d2098d958088762d09c3c9a7da78ee3db8c
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-12-11 14:01:52 +0300

    add imports

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,12 @@
 'use client';
 
+import { motion } from 'framer-motion';
+import { socials } from '../constants';
+
+import styles from '../styles';
+import { footerVariants } from '../utils/motion';
+
+
 const Footer = () => (
   <footer>
     footer
