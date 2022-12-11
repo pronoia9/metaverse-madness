19bc264f2c0fc68eedcd574f3eeb7572989efc57
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-12-11 14:27:58 +0300

    add title

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -14,6 +14,12 @@ const Footer = () => (
     initial='hidden'
     whileInView='show'>
     <div className='footer-gradient' />
+
+    <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
+      <div className='flex items-center justify-between flex-wrap gap-5'>
+        <h4 className='font-bold md:text-[64px] text-[44px] text-white'>Enter the Metaverse</h4>
+      </div>
+    </div>
   </motion.div>
 );
 
