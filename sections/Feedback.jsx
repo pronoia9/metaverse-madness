05e46f7032ef06874acead6d4cc2c187e87e65a5
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-12-11 13:34:29 +0300

    add imports

--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -1,5 +1,10 @@
 'use client';
 
+import { motion } from 'framer-motion';
+
+import styles from '../styles';
+import { fadeIn, staggerContainer, zoomIn } from '../utils/motion';
+
 const Feedback = () => (
   <section>
     Feedback section
